982861b5004cfc8147553332818a6ca8220b8183
lrborges0101 <leoloco40@gmail.com>
2023-01-03 13:49:58 +0000

    first commit

---
+++ pages/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Test.module.scss'
+
+const Test = () => {
+  return (
+    <div className={style.teste} >
+      teste
+    </div>
+  )
+}
+
+export default Test
